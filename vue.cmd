c0a0fdcb94c070caeb919f4551bc3bd0f55096f2
shubham digole <shubhamdigole@gmail.com>
2020-01-03 18:32:24 +0530

    second failed output

--- vue.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\@vue\cli\bin\vue.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\@vue\cli\bin\vue.js" %*
-)
